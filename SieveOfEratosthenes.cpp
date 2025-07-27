33d66c2e04d3f174939ec968a2dccfe05329b9d4
JMasson0706 <james.masson070606@gmail.com>
2025-07-27 19:39:39 +0800

    Add files via upload

---
+++ SieveOfEratosthenes.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+// Handles user input
+const int getInt() 
+{
+    std::cout << "Enter number: \n";
+    int num{};
+    std::cin >> num;
+    return num;
+}
+
+std::vector<int> primesUnderN(int num) 
+{
+    int isPrime[num];
+	
+	for (int i{ 0 }; i < num; ++i) {
+	    isPrime[i] = 1;
+	}
+
+    const int limit = static_cast<int>(std::sqrt(num));
+    
+    // marks all multiples of # as false
+    for (int i{ 2 }; i < limit; ++i) 
+    {
+        for (int j{ i * i }; j < num; j += i) 
+        {
+            isPrime[j] = 0;
+        }
+    }
+    
+    std::vector<int> primes{};
+    // Iterates through array marking all true indices as primes
+    for (int i{ 1 }; i < num; ++i){
+        if (isPrime[i] == 1) primes.push_back(i);
+    }
+    
+    return primes;
+}
+
+int main() 
+{
+    
+	const int num{ getInt() };
+    
+    std::vector<int> primes{ primesUnderN(num) };
+    
+    
+    // ouput primes 
+    for (int p : primes) {
+        std::cout << p << ' ';
+    }
+    
+    std::cout << "\n";
+    return 0;
+    
+}
